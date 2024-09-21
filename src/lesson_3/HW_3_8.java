2e8971c683cefee3b0312bd4f35db458d172c0ac
Egor-Shi <gray-1592@mail.ru>
2024-09-21 16:47:04 +0300

    Переделал домашнее задание

--- src/lesson_3/HW_3_8.java
+++ src/lesson_3/HW_3_8.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class HW_3_8 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+
+Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
-if (a <= 3){
+        if(a <= 3){
     System.out.println("Горит зеленый");
 }
 else if(a > 3 && a <= 4){
     System.out.println("Горит желтый");
 }
-else if(a > 4 && a<=5){
+else if(a > 4 && a <= 5){
     System.out.println("Горит красный");
 }
 else if (a % 5 <= 3){
